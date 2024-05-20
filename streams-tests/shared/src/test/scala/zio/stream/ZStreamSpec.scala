4442714a0d5a6743313ceefa4af904c882603b32
chintu-777 <160070339+chintu-777@users.noreply.github.com>
2024-05-20 16:44:18 +0530

    Update ZStreamSpec.scala

--- streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
+++ streams-tests/shared/src/test/scala/zio/stream/ZStreamSpec.scala
@@ -3725,7 +3725,7 @@ object ZStreamSpec extends ZIOBaseSpec {
               _      <- stream.tapSink(sink).take(3).runDrain
               result <- ref.get
             } yield assertTrue(result == 6)
-          }
+          },
           test("ensure complete processing with early termination") {
             for {
               ref    <- Ref.make(0)
