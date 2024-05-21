48a6dc80484dddc67c76463e05802de8684c1436
chintu-777 <160070339+chintu-777@users.noreply.github.com>
2024-05-22 00:34:50 +0530

    Update ZStream.scala

--- streams/shared/src/main/scala/zio/stream/ZStream.scala
+++ streams/shared/src/main/scala/zio/stream/ZStream.scala
@@ -3278,32 +3278,32 @@ def tapSink[R1 <: R, E1 >: E](
       ZChannel.readWithCause(
         chunk =>
           ZChannel
-            .fromZIO(queue.offer(Take.chunk(chunk)))
+            .fromZIO(queue.offer(Take.chunk(chunk)).tap(_ => ZIO.debug(s"Offered chunk: $chunk")))
             .foldCauseChannel(
               _ => ZChannel.write(chunk) *> ZChannel.identity,
               _ => ZChannel.write(chunk) *> loop
             ),
         cause =>
           ZChannel
-            .fromZIO(queue.offer(Take.failCause(cause)))
+            .fromZIO(queue.offer(Take.failCause(cause)).tap(_ => ZIO.debug(s"Offered fail cause: $cause")))
             .foldCauseChannel(
               _ => ZChannel.refailCause(cause),
               _ => ZChannel.refailCause(cause)
             ),
         _ =>
           ZChannel
-            .fromZIO(queue.offer(Take.end))
+            .fromZIO(queue.offer(Take.end).tap(_ => ZIO.debug("Offered end")))
             .foldCauseChannel(
               _ => ZChannel.unit,
               _ => ZChannel.unit
             )
       )
     new ZStream(
-      ZChannel.fromZIO(promise.await) *> self.channel
+      ZChannel.fromZIO(promise.await).tap(_ => ZIO.debug("Promise awaited")) *> self.channel
         .pipeTo(loop)
-        .ensuring(queue.offer(Take.end) *> queue.awaitShutdown) *> ZChannel.unit
+        .ensuring(queue.offer(Take.end).tap(_ => ZIO.debug("Ensuring end offered")).fork *> queue.awaitShutdown.tap(_ => ZIO.debug("Queue shutdown awaited"))) *> ZChannel.unit
     )
-      .merge(ZStream.execute((promise.succeed(()) *> right.run(sink).ensuring(queue.shutdown)).ensuring(promise.await)), HaltStrategy.Both)
+      .merge(ZStream.execute((promise.succeed(()).tap(_ => ZIO.debug("Promise succeeded")) *> right.run(sink).ensuring(queue.shutdown.tap(_ => ZIO.debug("Queue shutdown")))).ensuring(promise.await.tap(_ => ZIO.debug("Promise awaited in merge")))), HaltStrategy.Both)
   }
 
   /**
